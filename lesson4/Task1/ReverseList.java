dd1ee696e408dc90ff9090f6f5a4951939548dee
roman <tsoyroman29@gmail.com>
2022-12-31 12:55:55 +0900

    Change code

--- lesson4/Task1/ReverseList.java
+++ lesson4/Task1/ReverseList.java
@@ -1,16 +1,29 @@
 package lesson4.Task1;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 
 public class ReverseList {
     public static void main(String[] args) {
-        LinkedList<Integer>ll = new LinkedList<>(Arrays.asList(1,2,3,4,5));
-
+        List<Integer>ll = new LinkedList<Integer>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        
         System.out.printf("Исходный список %s\n",ll);
-        Collections.reverse(ll);    
-        System.out.printf("Перевернутый список %s\n",ll);
+        List<Integer>reverse = reversell(ll);
+        System.out.printf("Перевернутый список %s\n",reverse);
     }
+    public static List<Integer> reversell(List<Integer>ll){
+        
+        for(int i = ll.size()-1;i >= 0;i--){
+                ll.add(ll.get(i));
+                ll.remove(i);
+        }  
+        return ll;    
+        }
+        
 }
+
--- lesson4/Task2/QueueMethod.java
+++ lesson4/Task2/QueueMethod.java
@@ -1,38 +1,35 @@
 package lesson4.Task2;
-import java.util.Collections;
-import java.util.Deque;
+
 /*Реализуйте очередь с помощью LinkedList со следующими методами:enqueue() - помещает элемент в конец очереди, 
 dequeue() - возвращает первый элемент из очереди и удаляет его, first() - возвращает первый элемент из очереди, не удаляя. */
 import java.util.LinkedList;
-import java.util.Queue;
+
 
 public class QueueMethod {
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<Integer>();
+        LinkedList<Integer> queue = new LinkedList<Integer>();
         queue.add(1);
         queue.add(2);
         queue.add(5);
         System.out.printf("Исходный список %s\n", queue);
         enqueue(queue, 6);
-        System.out.printf("Cписок после добавления элементв в конец очереди %s\n", queue);
-        int a = enqueue(queue);
-        System.out.printf("Cписок после удаления первого в очереди элемента (%d) %s\n",a, queue);
+        System.out.printf("Cписок после добавления элемента в конец очереди %s\n",queue);
+        int a = dequeue(queue);
+        System.out.printf("первый элемент %d\n",a);
+        System.out.printf("Cписок после удаления первого в очереди элемента  %s\n", queue);
         int b = first(queue);
         System.out.printf("Первый элемент в очереди %d \n",b);
-
-
-        
-        
+        System.out.printf("Список %s \n",queue);     
 
     }
-    public static Queue<Integer>enqueue(Queue<Integer> queue,int a){
-        queue.offer(a);
+    public static LinkedList<Integer>enqueue(LinkedList<Integer> queue,int a){
+        queue.add(queue.size(), a);
         return queue;
     }
-    public static int enqueue(Queue<Integer> queue){
-        return queue.remove();
+    public static int dequeue(LinkedList<Integer> queue){
+        return queue.removeFirst();
     }
-    public static int first(Queue<Integer> queue){
+    public static int first(LinkedList<Integer> queue){
         return queue.element();
     }
 
